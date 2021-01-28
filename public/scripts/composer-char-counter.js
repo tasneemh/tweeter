39c29417ee32e23f6aa7c6f8145654f763f990c2
Tasneem Burmawala <tasneemhburmawala@gmail.com>
2021-01-28 17:27:23 +0000

    Tweeter app created

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,23 @@
+ $(document).ready(function(){
+   //gets value of counter
+   //let charLeft = $(".counter").val();
+  $("#tweet-text").on("input", function(){
+    
+    //charLeft--;
+    //sets value of the counter
+    //$(".counter").val(charLeft);
+    
+    //if value of counter is less than 0
+    if (140 - $(this).val().length < 0){
+      $(".counter").addClass("red-color")
+        .val(140 - $(this).val().length);
+    } else {
+      $(".counter").removeClass("red-color")
+        .val(140 - $(this).val().length);
+    }
+  });
+
+ });
+
+ 
+   
