05d7997f7e74832e3910624b896b4c1f3b8b41f2
Tasneem Burmawala <tasneemhburmawala@gmail.com>
2021-01-29 02:40:29 +0000

    adding all the files

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,41 +4,18 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// const data = [
-//     {
-//       "user": {
-//         "name": "Newton",
-//         "avatars": "https://i.imgur.com/73hZDYK.png"
-//         ,
-//         "handle": "@SirIsaac"
-//       },
-//       "content": {
-//         "text": "If I have seen further it is by standing on the shoulders of giants"
-//       },
-//       "created_at": 1461116232227
-//     },
-//     {
-//       "user": {
-//         "name": "Descartes",
-//         "avatars": "https://i.imgur.com/nlhLi3I.png",
-//         "handle": "@rd" },
-//       "content": {
-//         "text": "Je pense , donc je suis"
-//       },
-//       "created_at": 1461113959088
-//     }
-//   ]
-$(document).ready(function(){
+$(document).ready(function() {
   //hides the error message html element
-$('.errorMsg').hide();
-//converts date into days ago
-const convertDate = function(d){
-  const daysSinceTweet = (Date.now() - d) / 86400000;
-  return Math.round(daysSinceTweet);
-}
+  $('.errorMsg').hide();
+  //converts date into days ago
+  const convertDate = function(d) {
+    const daysSinceTweet = (Date.now() - d) / 86400000;
+    return Math.round(daysSinceTweet);
+  };
 
- const createTweetElement = function(tweetData){
-   const result = `<article class="tweet-article">
+  //function to create tweet
+  const createTweetElement = function(tweetData) {
+    const result = `<article class="tweet-article">
                       <header class="tweet-header">
                         <div>
                           <img src=${tweetData.user.avatars} alt="">
@@ -57,44 +34,42 @@ const convertDate = function(d){
                     </div>
                     </footer>
                     </article>`;
-                    //result is a tweet object
-                    return result;
- };
+    //result is a tweet object
+    return result;
+  };
 
- // to add it to the page so we can make sure it's got all the right elements, classes, etc.
- 
-
-const renderTweets = function(tweets){
+  // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  const renderTweets = function(tweets) {
   //tweets is an array of tweet objects
-  for (let tweet of tweets){
+    for (let tweet of tweets) {
     // remove any tweets that are already in the container
-  //$('#tweets-container').empty();
+      //$('#tweets-container').empty();
     //create new tweet element
-    const newTweet = createTweetElement(tweet);
-    //append each new tweet to the tweets-container
-    $('#tweets-container').prepend(newTweet);
-    $('#text').text(tweet.content.text);
-    $('#name').text(tweet.user.name);
-    $('#tweet-name').text(tweet.user.handle);
-    $('#tweet-text').val('');
-  }
-};
-const loadTweets =function(){
-  $.ajax({
-    url: "/tweets",
-    method: 'GET',
-  }).done((result)=>{
-    renderTweets(result);
-  }).fail(()=>{
-    console.log(`Error`);
-  }).always(()=>{
-    console.log(`Request is completed`);
-  })
-};
-
+      const newTweet = createTweetElement(tweet);
+      //append each new tweet to the tweets-container
+      $('#tweets-container').prepend(newTweet);
+      $('#text').text(tweet.content.text);
+      $('#name').text(tweet.user.name);
+      $('#tweet-name').text(tweet.user.handle);
+      $('#tweet-text').val('');
+    }
+  };
+  
+  const loadTweets = function() {
+    $.ajax({
+      url: "/tweets",
+      method: 'GET',
+    }).done((result)=>{
+      renderTweets(result);
+    }).fail(()=>{
+      console.log(`Error`);
+    }).always(()=>{
+      console.log(`Request is completed`);
+    });
+  };
 
   //event handler for the form with id tweet-form and submit event
-  $('#tweet-form').on('submit', function(event){
+  $('#tweet-form').on('submit', function(event) {
     // prevent the default form submission
     event.preventDefault();
     // Read the data from the input text content
@@ -103,29 +78,32 @@ const loadTweets =function(){
     const tweetEle = searchBox.val();
     //extract the information from the form => serialize
     const formContent = $(this).serialize();
-    if (tweetEle.length > 140){
+    if (tweetEle.length > 140) {
       //shows the error message html element
       //$('.errorMsg').show();
       $('.error-text').text("Please enter only 140 characters");
       $('.errorMsg').slideDown(500);
-    } else if (tweetEle === "" || tweetEle === null){
-       //shows the error message html element
+    } else if (tweetEle === "" || tweetEle === null) {
+      //shows the error message html element
       //$('.errorMsg').show();
       $('.error-text').text("Please do not submit empty tweet");
       $('.errorMsg').slideDown(500);
     } else {
       //adding the information to the array of tweets objects
       $.ajax({
-      url: `/tweets`,
-      method: 'POST',
-      data: formContent
+        url: `/tweets`,
+        method: 'POST',
+        data: formContent
       }).done(()=>{
-      $('.errorMsg').slideUp(0);
-      loadTweets();
+        $('.errorMsg').slideUp(0);
+        loadTweets();
       }).fail(()=>{
-      console.log(`Error`);
+        console.log(`Error`);
       });
-    }        
+    }
+  });
+  loadTweets();
 });
-loadTweets();
-});
+
+
+
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,14 @@
- $(document).ready(function(){
-   //gets value of counter
-   //let charLeft = $(".counter").val();
-  $("#tweet-text").on("input", function(){
+$(document).ready(function() {
+  //gets value of counter
+  //let charLeft = $(".counter").val();
+  $("#tweet-text").on("input", function() {
     
     //charLeft--;
     //sets value of the counter
     //$(".counter").val(charLeft);
     
     //if value of counter is less than 0
-    if (140 - $(this).val().length < 0){
+    if (140 - $(this).val().length < 0) {
       $(".counter").addClass("red-color")
         .val(140 - $(this).val().length);
     } else {
@@ -17,7 +17,7 @@
     }
   });
 
- });
+});
 
  
    
